c921e9b11199181fdfce11121d1ea002141c159c
[blay] <[argyosfbalay@gmail.com]>
2018-12-14 20:54:26 +0200

    fix nev

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { OperationsFinancialAccountComponent } from './fbm-main-app/module/fbm-menu/operations-financial-account/operations-financial-account.component';
-import { TrafficInInstallmentsComponent } from './fbm-main-app/module/fbm-menu/operations-financial-account/traffic-in-installments/traffic-in-installments.component';
 import { TrackFinancialAccountComponent } from './fbm-main-app/module/fbm-menu/track-financial-account/track-financial-account.component';
 
 const routes: Routes = [
