79f0cfcd21ce50b13194dec440cdef6b8ba7f05d
[blay] <[argyosfbalay@gmail.com]>
2018-12-14 20:49:20 +0200

    move files : "manue and nev " files  (14.12.18 )
    הז

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { OperationsFinancialAccountComponent } from './fbm-main/menu/operations-financial-account/operations-financial-account.component';
-import { TrackFinancialAccountComponent } from './fbm-main/menu/track-financial-account/track-financial-account.component';
-import { TrafficInInstallmentsComponent } from './fbm-main/menu/operations-financial-account/traffic-in-installments/traffic-in-installments.component';
- 
+import { OperationsFinancialAccountComponent } from './fbm-main-app/module/fbm-menu/operations-financial-account/operations-financial-account.component';
+import { TrafficInInstallmentsComponent } from './fbm-main-app/module/fbm-menu/operations-financial-account/traffic-in-installments/traffic-in-installments.component';
+import { TrackFinancialAccountComponent } from './fbm-main-app/module/fbm-menu/track-financial-account/track-financial-account.component';
+
 const routes: Routes = [
   //next teb 1
   { path: '', redirectTo: '/operationsFinancialAccount', pathMatch: 'full' },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from 'src/app/app.component';
 import { AppRoutingModule, MenuRoutingComponents } from './app-routing.module';
-import { fbmMainComponent } from './core/fbmMain/fbm-main.component';
- 
+import { fbmMainComponent } from './core/main-app/menu-app/fbm-main.component';
 
 @NgModule({
   declarations: [
