826eb12f4967f00de2b0930d5cf3ca9e5171a329
[blay] <[argyosfbalay@gmail.com]>
2018-12-18 23:19:47 +0200

    aa

--- src/app/core/app-module/app-routing.module.ts
+++ src/app/core/app-module/app-routing.module.ts
@@ -3,6 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { OperationsFinancialAccountComponent } from 'src/app/fbm-module-app/menu-module/fbm-menu/operations-financial-account/operations-financial-account.component';
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+import { TrafficInInstallmentsComponent } from 'src/app/fbm-module-app/menu-module/fbm-menu/operations-financial-account/nev-path/traffic-in-installments/traffic-in-installments.component';
+>>>>>>> parent of 7543c2a...  fix files: set new file header,nav section.... (15.12.18 ~ v 0.0.5)
 =======
 import { TrafficInInstallmentsComponent } from 'src/app/fbm-module-app/menu-module/fbm-menu/operations-financial-account/nev-path/traffic-in-installments/traffic-in-installments.component';
 >>>>>>> parent of 7543c2a...  fix files: set new file header,nav section.... (15.12.18 ~ v 0.0.5)
